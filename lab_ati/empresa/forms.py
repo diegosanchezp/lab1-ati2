2d1b3766c663d34c140078a86fb0f1e48ced02b3
Diego Sánchez <diegosandmg@gmail.com>
2022-07-11 16:17:53 -0400

    refactor views and finish form work create.html

--- lab_ati/empresa/forms.py
+++ lab_ati/empresa/forms.py
@@ -1,45 +1,40 @@
 from django import forms
 
+from lab_ati.empresa.models import Empleado
+
 COUNTRY_CHOICES = (
     ("0", "Selecciona..."),
     ("ve", "Venezuela"),
-    ("ec", "Ecuardo"),
+    ("ec", "Ecuador"),
     ("es", "España"),
 )
 
 
 # creating a form
-class CreateEmployeeForm(forms.Form):
-    nombre = forms.CharField(
-        label="Nombre",
-        required=True,
-        error_messages={
-            "required": "El campo nombre es requerido",
-        },
-        widget=forms.TextInput(
-            attrs={
-                "id": "nombre",
-                "name": "nombre",
-                "class": "form-control",
-                "placeholder": "Inserte un nombre",
-            }
-        ),
-    )
-    apellidos = forms.CharField(
-        label="Apellidos",
-        required=True,
-        error_messages={
-            "required": "El campo apellidos es requerido",
-        },
-        widget=forms.TextInput(
-            attrs={
-                "id": "apellidos",
-                "name": "apellidos",
-                "class": "form-control",
-                "placeholder": "Inserte apellidos",
-            }
-        ),
-    )
+class CreateEmployeeForm(forms.ModelForm):
+    """
+    Formulario del modelo empleado
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args,**kwargs)
+        # Set custom widget attr
+        for field in  self.fields.values():
+            if isinstance(field, forms.fields.CharField):
+                if isinstance(field.widget, forms.widgets.Textarea):
+                    field.widget = forms.TextInput(attrs={
+                        "required": True,
+                    })
+
+                field.widget.attrs.update({
+                    "class": "form-control",
+                })
+
+            if isinstance(field.widget, forms.widgets.Select):
+                field.widget.attrs.update({
+                    "class": "form-select",
+                })
+
+    # Override pais field
     pais = forms.ChoiceField(
         label="Pais",
         required=True,
@@ -50,8 +45,13 @@ class CreateEmployeeForm(forms.Form):
             attrs = {
                 "id": "pais",
                 "name": "pais",
-                "class": "form-control",
+                "class": "form-select",
             },
         ),
         choices = COUNTRY_CHOICES
     )
+
+    class Meta:
+        model = Empleado
+        fields = "__all__"
+
--- lab_ati/empresa/urls.py
+++ lab_ati/empresa/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = "empresa"
 
 urlpatterns = [
-    path("employees/create", views.CreateEmployeeView, name="create-employee")
+    path("employees/create", views.CreateEmployeeView.as_view(), name="create-employee"),
+    path("employees/edit/<slug:pk>", views.EditEmployeeView.as_view(), name="edit-employee")
 ]
