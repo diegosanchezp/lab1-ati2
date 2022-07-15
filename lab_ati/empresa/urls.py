c5a62d15099fce21a68ffcb3beaccfd9f82fdca8
Aarón Morillo <aaron@amobasoftware.com>
2022-07-15 16:20:46 -0400

    added new design for employee section

--- lab_ati/empresa/urls.py
+++ lab_ati/empresa/urls.py
@@ -1,3 +1,4 @@
+from audioop import reverse
 from django.urls import path
 
 from . import views
@@ -5,6 +6,10 @@ from . import views
 app_name = "empresa"
 
 urlpatterns = [
-    path("employees/create", views.CreateEmployeeView.as_view(), name="create-employee"),
-    path("employees/edit/<slug:pk>", views.EditEmployeeView.as_view(), name="edit-employee")
+    path("<slug:business_id>/employees/create", views.CreateEmployeeView.as_view(), name="create-employee"),
+    path("<slug:business_id>/employees/edit/<slug:pk>", views.EditEmployeeView.as_view(), name="edit-employee"),
+    path("<slug:business_id>/employees/", views.ListEmployeeView.as_view(), name="list-employee"),
+    path("<slug:business_id>/employees/delete/<slug:pk>", views.DeleteEmployeeView.as_view(), name="delete-employee"),
+    path("<slug:business_id>/employees/<slug:pk>", views.DetailEmployeeView.as_view(), name="detail-employee")
+
 ]
