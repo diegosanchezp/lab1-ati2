0d8969bf9fe494f3dcb9903cf84e14c045cf212d
katherin mozo <kathemozo27@gmail.com>
2022-07-16 21:03:38 -0400

    create y update de proveedor sin rs

--- lab_ati/proveedor/models.py
+++ lab_ati/proveedor/models.py
@@ -6,17 +6,21 @@ from django.contrib.contenttypes.fields import GenericRelation
 # Create your models here.
 
 class Proveedor(EmpresaABC):
-    representante=models.TextField(_("Representante"))
-    cargo=models.TextField(_("Cargo"))
-    email_representante=models.EmailField(_("Correo del representante de  la empresa"))
-    email_personal_representante=models.EmailField(_("Correo personal del representante"))
     tlf=models.TextField(_("Teléfono del proveedor"))
+    representante=models.TextField(_("Nombre del representante"))
+    cargo=models.TextField(_("Cargo del representante"))
+    email_representante=models.EmailField(_("Correo del representante"))
+    email_personal_representante=models.EmailField(_("Correo personal del representante"))
     tlf_representate=models.TextField(_("Teléfono celular del representante"))
-    tlf_local=models.TextField(_("Teléfono local"))
-    pais_representante=models.TextField(_("Pais de residencia"))
+    tlf_local=models.TextField(_("Teléfono local del representante"))
+    pais_representante=models.TextField(_("Pais de residencia de representante"))
     redes_representante=GenericRelation(
         to="empresa.SocialMedia",
         verbose_name=_("Redes sociales del representante"),
+    )
+    redes_proveedor=GenericRelation(
+        to="empresa.SocialMedia",
+        verbose_name=_("Redes sociales del proveedor"),
     )
     empresa=models.ForeignKey(
         to="empresa.Empresa",
--- lab_ati/proveedor/views.py
+++ lab_ati/proveedor/views.py
@@ -1,12 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Proveedor
 from .forms import ProveedorForm
 from ..empresa.models import Empresa
+from django.template.defaulttags import register
 
+@register.filter
+def get_item(objectList, key):
+  if objectList == "":
+    return ""
+  return getattr(objectList,key)
 
 def createProveedor(request):
-  if request.method == 'GET':
+  if request.method == 'GET': 
     empresaId = request.GET.get('empresa') 
     if(empresaId == None):
       return render(request,'404.html')
@@ -14,17 +20,61 @@ def createProveedor(request):
     if(empresa == None):
       return render(request,'404.html')
     print(empresa)
+    formularioProveedor = ProveedorForm(request.POST or request.GET)
     context = {}
-    context["data"] = { "empresa":{"id":empresaId,"nombre":empresa.nombre},"update":True}
-    return render(request,'proveedor/create-update.html')
+    context["data"] = { "empresa":empresa,"update":False, "formulario":formularioProveedor}
+    return render(request,'pages/proveedor/create-update.html', context)
+
   elif request.method == 'POST':
+    empresaId = request.GET.get('empresa') 
+    if(empresaId == None):
+      return render(request,'404.html')  
+    empresa = Empresa.objects.get(id=empresaId)
+    if(empresa == None):
+      return render(request,'404.html')
     formularioProveedor = ProveedorForm(request.POST or None)
+    print(formularioProveedor)
     if formularioProveedor.is_valid():
+      print('is valid!')
       formularioProveedor.save()
-      return HttpResponse('guardado!')
-    return render(request,'proveedor/create-update.html')
+    return redirect('/proveedor?empresa='+empresaId)
     
-    
+def updateProveedor(request):
+  if request.method == 'GET':
+    proveedorId = request.GET.get('proveedor')
+    print('proveedor id: ', proveedorId)
+    if(proveedorId == None):
+      return render(request,'404.html')
+    proveedor = Proveedor.objects.get(id=proveedorId)
+    if(proveedor == None):
+      return render(request,'404.html') 
+    print(proveedor)
+    formularioProveedor = ProveedorForm(request.POST or request.GET)
+    context = {
+      "data":{
+        "proveedor":proveedor,
+        "empresa":{"id":proveedor.empresa.id},
+        "update":True,
+        "formulario":formularioProveedor
+      }
+    }
+    return render(request,'pages/proveedor/create-update.html',context)
+  elif request.method == 'POST':
+    proveedorId = request.GET.get('proveedor')
+    print('proveedor id: ', proveedorId)
+    if(proveedorId == None):
+      return render(request,'404.html')
+    oldProveedor = Proveedor.objects.get(id=proveedorId)
+    if(oldProveedor == None):
+      return render(request,'404.html')
+    print(oldProveedor)
+    formularioProveedor = ProveedorForm(request.POST, instance=oldProveedor)
+    print(formularioProveedor)
+    empresaId = str(oldProveedor.empresa.id)
+    if formularioProveedor.is_valid():
+      print('is valid!')
+      formularioProveedor.save()
+    return redirect('/proveedor?empresa='+empresaId)
 
 
 #listar proeedores de una emrpresa dada
@@ -32,12 +82,12 @@ def listProveedor(request):
   empresaId = request.GET.get('empresa')
   print(empresaId)
   if(empresaId == None):
-    return render(request,'404.html')
+    return render(request,'pages/404.html')
 
   empresa = findEmpresa(empresaId)
   print(empresa)
   if(empresa == None): #empresa no existe
-    return render(request,'404.html')
+    return render(request,'pages/404.html')
 
   proveedoresList = Proveedor.objects.filter(empresa__id__contains=empresaId)
   print(proveedoresList)
@@ -49,58 +99,33 @@ def listProveedor(request):
 
   context["list"] = proveedoresList
   context["empresa"] = {"empresa":empresa, "id":empresaId}
-  return render(request,'proveedor/list.html',context)
+  return render(request,'pages/proveedor/list.html',context)
   
 
-def updateProveedor(request):
-  if request.method == 'GET':
-    proveedorId = request.GET.get('proveedor')
-    print('proveedor id: ', proveedorId)
-    if(proveedorId == None):
-      return render(request,'404.html')
-    proveedor = Proveedor.objects.get(id=proveedorId)
-    if(proveedor == None):
-      return render(request,'404.html')
-    print(proveedor)
-    context = {
-      "data":{
-        "proveedor":proveedor,
-        "update":True
-      }
-      
-    }
-    print(context)
-    return render(request,'proveedor/create-update.html',context)
-  elif request.method == 'POST':
-    proveedorId = request.GET.get('proveedor')
-    print('proveedor id: ', proveedorId)
-    if(proveedorId == None):
-      return render(request,'404.html')
-    oldProveedor = Proveedor.objects.get(id=proveedorId)
-    if(oldProveedor == None):
-      return render(request,'404.html')
+
 
 def deleteProveedor(request):
   proveedorId = request.GET.get('proveedor') 
   if(proveedorId == None):
-    return render(request,'404.html')
+    return render(request,'pages/404.html')
   proveedor = Proveedor.objects.get(id=proveedorId)
   if(proveedor == None):
-    return render(request,'404.html')
+    return render(request,'pages/404.html')
   proveedor.delete()
-  return render(request,'proveedor/list.html')
+  empresaId = str(proveedor.empresa.id)
+  return redirect('/proveedor?empresa='+empresaId)
 
 def seeProveedor(request):
   proveedorId = request.GET.get('proveedor') 
   if(proveedorId == None):
-    return render(request,'404.html')
+    return render(request,'pages/404.html')
   proveedor = Proveedor.objects.get(id=proveedorId)
   if(proveedor == None):
-    return render(request,'404.html')
+    return render(request,'pages/404.html')
   context = {
     "proveedor" :proveedor,
   }
-  return render(request,'proveedor/read.html', context)
+  return render(request,'pages/proveedor/read.html', context)
 
 ##otras funciones
 def findEmpresa(empresaId):
